21d4e5d6c0d63afb8d36ac5bc76b9f3632fed242
Ronald Pranata <ronald_pranata@yahoo.co.id>
2022-06-07 20:41:42 +0800

    update cypress

--- cypress/specs/test.js
+++ cypress/specs/test.js
@@ -3,54 +3,7 @@ import * as _ from 'lodash';
 
 import mockCatBreeds from '../../src/__mock__/mock';
 
-const cat = {
-  adaptability: 5,
-  affection_level: 5,
-  alt_names: '',
-  cfa_url: 'http://cfa.org/Breeds/BreedsAB/Abyssinian.aspx',
-  child_friendly: 3,
-  country_code: 'EG',
-  country_codes: 'EG',
-  description: 'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.',
-  dog_friendly: 4,
-  energy_level: 5,
-  experimental: 0,
-  grooming: 1,
-  hairless: 0,
-  health_issues: 2,
-  hypoallergenic: 0,
-  id: 'abys',
-  image: {
-    height: 1445,
-    id: '0XYvRd7oD',
-    url: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
-    width: 1204,
-  },
-  indoor: 0,
-  intelligence: 5,
-  lap: 1,
-  life_span: '14 - 15',
-  name: 'Abyssinian',
-  natural: 1,
-  origin: 'Egypt',
-  rare: 0,
-  reference_image_id: '0XYvRd7oD',
-  rex: 0,
-  shedding_level: 2,
-  short_legs: 0,
-  social_needs: 5,
-  stranger_friendly: 5,
-  suppressed_tail: 0,
-  temperament: 'Active, Energetic, Independent, Intelligent, Gentle',
-  vcahospitals_url: 'https://vcahospitals.com/know-your-pet/cat-breeds/abyssinian',
-  vetstreet_url: 'http://www.vetstreet.com/cats/abyssinian',
-  vocalisation: 1,
-  weight: {
-    imperial: '7  -  10',
-    metric: '3 - 5',
-  },
-  wikipedia_url: 'https://en.wikipedia.org/wiki/Abyssinian_(cat)',
-};
+const cat = mockCatBreeds[0];
 
 describe('Given I am a user of a cat web application', () => {
   it('see a page with a table that contains multiple cat breeds', () => {
@@ -75,11 +28,11 @@ describe('Given I am a user of a cat web application', () => {
   });
   it('I click on a specific entry on the data table', () => {
     cy.get('.body')
-      .get('#Abyssinian')
+      .get(`#${cat.name}`)
       .click();
   });
   it('Then I should be directed to a page of the chosen cat breed', () => {
-    cy.location('pathname').should('eq', '/catDetail/Abyssinian');
+    cy.location('pathname').should('eq', `/catDetail/${cat.name}`);
   });
   it('And I should see the breed title, image, description, maximum weight, average lifespan', () => {
     cy.get('.title')
