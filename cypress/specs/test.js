105cd8d6b8e3fcd326493f7805ae731ebcb54ef7
Ronald Pranata <ronald_pranata@yahoo.co.id>
2022-06-07 20:31:01 +0800

    update cypress test

--- cypress/specs/test.js
+++ cypress/specs/test.js
@@ -3166,53 +3166,51 @@ const cat = {
     metric: '3 - 5',
   },
   wikipedia_url: 'https://en.wikipedia.org/wiki/Abyssinian_(cat)',
-}
+};
 
 describe('Given I am a user of a cat web application', () => {
   it('see a page with a table that contains multiple cat breeds', () => {
-    const header = ['Image','Name'];
+    const header = ['Image', 'Name'];
     const body = _.map(mockCatBreeds, (o) => ({
       image: o?.image?.url,
       name: o.name,
     }));
 
     cy.visit('/');
-    header.forEach(function (value) {
-        cy.get('.header')
-          .get('.column')
-          .should('contain', value)
-    })
+    header.forEach((value) => {
+      cy.get('.header')
+        .get('.column')
+        .should('contain', value);
+    });
 
-    body.forEach((value)=>{
+    body.forEach((value) => {
       cy.get('.body')
         .get('.name')
-        .should('contain', value.name)
-  })
-  
+        .should('contain', value.name);
+    });
   });
   it('I click on a specific entry on the data table', () => {
-      cy.get('.body')
-        .get('#Abyssinian')
-        .click()
-  
+    cy.get('.body')
+      .get('#Abyssinian')
+      .click();
   });
   it('Then I should be directed to a page of the chosen cat breed', () => {
-    cy.location('pathname').should('eq', '/catDetail');
+    cy.location('pathname').should('eq', '/catDetail/Abyssinian');
   });
   it('And I should see the breed title, image, description, maximum weight, average lifespan', () => {
     cy.get('.title')
-      .should('contain',cat.name)
+      .should('contain', cat.name);
     cy.get('.description')
-      .should('contain',cat.description)
+      .should('contain', cat.description);
     cy.get('.cat-image')
-      .should('have.attr', 'src').should('include',cat.image.url)
+      .should('have.attr', 'src').should('include', cat.image.url);
     cy.get('.weight')
-      .should('contain',cat.weight.imperial)
+      .should('contain', cat.weight.imperial);
     cy.get('.life-span')
-      .should('contain',cat.life_span)
+      .should('contain', cat.life_span);
   });
   it('And I should see a map showing the origin of the cat breed', () => {
-    cy.get('.vue-map-container')                          // yields <nav>
-      .should('be.visible') 
+    cy.get('.vue-map-container') // yields <nav>
+      .should('be.visible');
   });
 });
